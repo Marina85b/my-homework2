7fedc158675eea5d8a83b099b90bc5cd5852f540
Neto Marina <koshkalisa2018@yandex.ru>
2025-02-27 18:48:03 +0300

    README

--- src/Main.java
+++ src/Main.java
@@ -2,27 +2,18 @@ public class Main {
     public static void main(String[] args) {
         int amount = 1100;
         int check = 100;
-        boolean isRefill = true;
+        int bonus = 0;
 
-        int bonus;
-        if (isRefill) {
-            bonus = 11;
-        } else {
-            bonus = 1;
 
-        }
-        int finalBonus = amount / 100;
-        if (finalBonus > 100) {
-            finalBonus = 100;
-        }
-        int balans = amount + finalBonus + check;
-        if (balans > 1100) ;
-
-        {
-
-            System.out.println("Количество бонусов:" + finalBonus);
-            System.out.println("Итоговый счет:" + balans);
+        if (amount > 1000) {
+            bonus = amount / 100;
         }
 
+        int finalBonus = bonus;
+        int balans = amount + finalBonus + check; 
+
+        // Вывод результата
+        System.out.println("Количество бонусов: " + finalBonus);
+        System.out.println("Итоговый счёт: " + balans);
     }
 }
